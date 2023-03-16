808d982200b4c593e27c164a7b7a6a993156917b
eeun95 <eeun95@gmail.com>
2023-03-17 00:00:14 +0900

    api 서버 만들기

--- domain/src/main/java/com/sek/ottfind/domain/dto/request/ReviewInsertRequestDto.java
+++ domain/src/main/java/com/sek/ottfind/domain/dto/request/ReviewInsertRequestDto.java
@@ -1,6 +1,7 @@
 package com.sek.ottfind.domain.dto.request;
 
 import com.sek.ottfind.domain.entity.Review;
+import com.sek.ottfind.domain.presentation.ReviewServiceDto;
 import lombok.Data;
 
 @Data
@@ -15,6 +16,15 @@ public class ReviewInsertRequestDto {
 
     public Review toEntity() {
         return Review.builder()
+                .url(this.url)
+                .starGrade(this.starGrade)
+                .comment(this.comment)
+                .build();
+    }
+
+    public ReviewServiceDto toServiceDto() {
+        return ReviewServiceDto.builder()
+                .ottContentId(this.ottContentId)
                 .url(this.url)
                 .starGrade(this.starGrade)
                 .comment(this.comment)
--- domain/src/main/java/com/sek/ottfind/domain/presentation/ReviewServiceDto.java
+++ domain/src/main/java/com/sek/ottfind/domain/presentation/ReviewServiceDto.java
@@ -1,10 +1,8 @@
 package com.sek.ottfind.domain.presentation;
 
 import com.sek.ottfind.domain.entity.OttContent;
-import com.sek.ottfind.domain.entity.Review;
 import lombok.Builder;
 import lombok.Getter;
-import org.hibernate.annotations.ColumnDefault;
 
 @Builder
 @Getter
@@ -12,7 +10,7 @@ public class ReviewServiceDto {
 
     private Long id;                // 시퀀스
 
-    private OttContent ottContent;  // ott
+    private Long ottContentId;      // ottId
 
     private String url;             // 리뷰 url
 
